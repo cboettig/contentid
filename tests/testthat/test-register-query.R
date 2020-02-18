b7c722c743fcafc264b391ac83a7ff0891211524
Carl Boettiger <cboettig@gmail.com>
2020-02-18 01:35:24 +0000

    more tests

---
+++ tests/testthat/test-register-query.R
@@ -0,0 +1,59 @@
+context("local register & query")
+
+test_that("We can register a URL in the local registry", {
+  skip_if_offline()
+  # Online tests sporadically time-out on CRAN servers
+  skip_on_cran() 
+  
+  x <- register_local("http://cdiac.ornl.gov/ftp/trends/co2/vostok.icecore.co2")
+  expect_is(x, "character")
+  expect_true(is_content_uri(x))
+})
+
+
+test_that("Error handling in registering a non-existent URL", {
+  
+  skip_if_offline()
+  skip_on_cran() 
+  
+  expect_error(
+    register_local("https://httpstat.us/404")
+  )
+  
+})
+
+
+test_that("We can register to multiple registries", {
+  
+  skip_if_offline()
+  skip_on_cran() 
+  
+  r1 <- tempfile()
+  dir.create(r1)
+  r2 <- tempfile()
+  dir.create(r2)
+
+    
+  x <- register("http://cdiac.ornl.gov/ftp/trends/co2/vostok.icecore.co2", registries = c(r1, r2))
+  y <- query("http://cdiac.ornl.gov/ftp/trends/co2/vostok.icecore.co2", registries = c(r1, r2))
+  
+  ## should be multiple entries from the multiple registries
+  expect_true(dim(y)[1] > 1)
+  
+  ## Should be exactly 1 entry for the URL in the temporary local registry 
+  y <- query("http://cdiac.ornl.gov/ftp/trends/co2/vostok.icecore.co2", registries = r1)
+  expect_true(dim(y)[1] == 1)
+  
+  ## Should be exactly 1 entry for the Content Hash in the temporary local registry 
+  y <- query(x, registries = r1)
+  expect_true(dim(y)[1] == 1)
+  
+  
+  ## clear out r1
+  unlink(r1)
+
+    
+})
+
+
+
