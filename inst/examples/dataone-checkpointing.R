b2f7547d69508e72b34907a19339362c09f4026b
Carl Boettiger <cboettig@gmail.com>
2020-04-29 23:22:58 +0000

    fix registries behavior when store=TRUE in resolve

--- inst/examples/dataone-checkpointing.R
+++ inst/examples/dataone-checkpointing.R
@@ -10,7 +10,13 @@ library(parallel)
 library(purrr)
 library(httr)
 
-ref <- contentid::resolve("hash://sha256/c7b8f1033213f092df630e9fc26cd6d941f2002c95ab829f0180903bc0cdcd50", store=TRUE)
+
+#########################
+
+### SKIP this block, start with pre-filtered version below ##
+
+## loads registered snapshot (see dataone.R)
+ref <- contentid::resolve("hash://sha256/598032f108d602a8ad9d1031a2bdc4bca1d5dca468981fa29592e1660c8f4883", registries = "/zpool/content-store")
 dataone <- vroom::vroom(ref, col_select = c(contentURL, baseURL)) %>% filter(!grepl("dryad", contentURL))
 
 ## Discover and prune failing domains
@@ -18,16 +24,31 @@ baseURLs <- dataone %>% select(baseURL) %>% distinct()
 resp <- map(baseURLs[[1]], purrr::safely(httr::GET, list(status_code = -1)))
 err_msg <- as.character(map(map(resp, "error"), "message"))
 problems <- data.frame(domain = baseURLs[[1]], err_msg, stringsAsFactors = FALSE) %>% filter(!grepl("^NULL", err_msg))
+dataone_good <- dataone %>% anti_join(select(problems, domain), by = c("baseURL" = "domain"))  %>% select(contentURL)
+#sum(grepl("usherbrooke", dataone$contentURL)) ## expect 0
 
 
-dataone <- dataone %>% anti_join(select(problems, domain), by = c("baseURL" = "domain")) 
-#sum(grepl("usherbrooke", dataone$contentURL))
+## Store clean snapshot: only the good contentURLs 
+readr::write_tsv(dataone_good, "dataone_good.tsv.gz")
+contentid::store("dataone_good.tsv.gz", "/zpool/content-store")
 
-rm(bases); rm(err_msg); rm(domain); rm(err); rm(problems); rm(resp); gc()
+rm(baseURLS); rm(resp); rm(err_msg); rm(problems);  gc()
 
-## All dryad is 404!!
+######################
+library(contentid)
+library(vroom)
+library(dplyr)
+library(parallel)
+library(purrr)
+library(httr)
+
+## Start with registered contentURL list and omit the ones already in registry:
+
+ref2 <- contentid::resolve("hash://sha256/2b961ccaac6068ed42ea34c1968d237af1b81cd16511ee0848e3ed5f0e573656", registries = "/zpool/content-store")
+dataone <- vroom::vroom(ref2, col_select = c(contentURL))
 
 done <- vroom::vroom("/zpool/content-store/data/registry.tsv.gz")
+
 contentURLs <- dplyr::anti_join(dataone, done, by = c(contentURL = "source"))[[1]]
 rm(dataone); rm(done); gc()
 
@@ -37,12 +58,16 @@ contentURLs <- contentURLs[!bad]
 
 p1 <- dplyr::progress_estimated(length(contentURLs))
 register_local_progress <- function(x){
-  #p1$tick()$print()
-  tryCatch(register(x,
+  p1$tick()
+  if(p1$i %% 5000 == 0) gc() # not clear that this helps any...
+  
+  tryCatch(
+    register(x,
                     "/zpool/content-store",
                     algos = c("md5","sha1","sha256")),
            error = function(e) NA_character_,
            finally = NA_character_)
+  
 }
 
 ### May run out of memory
